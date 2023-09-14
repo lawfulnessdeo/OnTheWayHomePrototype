74fc647e3b91366ca3ca4879334b94fb84162cf4
lawfulnessdeo <143490033+lawfulnessdeo@users.noreply.github.com>
2023-09-14 15:49:25 +0800

    Create events.py
    added events.py file

---
+++ events.py
@@ -0,0 +1,7 @@
+def default_options(health, sanity):
+    print("Health:", health ,"\nSanity:", sanity)
+    print("[1] Walk Forward\n[2] Items\n[3")
+def perform_event(x, m):
+    print("\n\nMETERS LEFT:" , (100 - m))
+    if x == 1:
+        print("\nYou are now outside the school gates. Your house is about 100 meters away.")
